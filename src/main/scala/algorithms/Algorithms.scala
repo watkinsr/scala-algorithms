1e22f8125a9ec61b484e4e0bf8baa27fec01fcd0
Ryan Watkins <ryanwatkins54@gmail.com>
2018-09-28 03:06:27 +0100

    init repo

---
+++ src/main/scala/algorithms/Algorithms.scala
@@ -0,0 +1,15 @@
+package algorithms
+
+object Algorithms extends App {
+  // Insertion sort
+  lazy val rand = List(5,3,1,2)
+  def isort(xs: List[Int]): List[Int] =
+    if (xs.isEmpty) Nil
+    else insert(xs.head, isort(xs.tail))
+
+  def insert(x: Int, xs: List[Int]): List[Int] =
+    if (xs.isEmpty || x <= xs.head) x :: xs
+    else xs.head :: insert(x, xs.tail)
+
+  println(isort(rand))
+}
